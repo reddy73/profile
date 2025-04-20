7c89509723c23addadfa26230d9def8626103fd3
Raghavendra Reddy <raghav@Raghavendras-MacBook-Pro.local>
2025-04-20 12:24:35 +0530

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/profile', // change if deploying to GitHub Pages
+  base: '/profile/', // change if deploying to GitHub Pages
 })
